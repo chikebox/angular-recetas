cd22274e9b66141ec13bdc32d4c2875bcbdfe580
Curso <Curso@IPARTEKAULA.COM>
2018-02-27 13:32:31 +0100

    Completar receta

---
+++ src/app/receta/receta.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receta',
+  templateUrl: './receta.component.html',
+  styleUrls: ['./receta.component.scss']
+})
+export class RecetaComponent implements OnInit {
+  nombre : string;
+  foto : string;
+  descripcion : string; 
+  likes : number;
+  cocinero : string;
+  fotoGluten: string;
+  isGlutenFree:boolean;
+  ingredientes:string[];
+  constructor() {
+    console.log('RecetaComponent constructor');
+    this.nombre='Bocata de calamares';
+    this.foto='https://www.hogarmania.com/archivos/201411/bocadillo-calamares-xl-668x400x80xX.jpg';
+    this.descripcion='soy un bocata de calamares';
+    this.likes=12; 
+    this.isGlutenFree=true;
+    this.cocinero="Carlos Arguiñano";
+    this.ingredientes=["Calamares","Limon","Pan","Salsa Ali Oli"]
+   }
+
+  ngOnInit() {
+    console.log('RecetaComponent ngOnInit');
+  }
+  sumarLike(){
+    this.likes++;
+  }
+}
